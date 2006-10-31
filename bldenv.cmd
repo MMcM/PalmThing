91af932a56a5dbf21098c3370c7590c815951eda
Mike McMahon <mmcm@comcast.net>
2006-10-31 14:27:57 +0000

    CVS_EXT for vc- commands in XEmacs.

--- bldenv.cmd
+++ bldenv.cmd
@@ -3,5 +3,6 @@ rem $Header$
 title Build PalmThing
 set JAVA_HOME=%~d0\jdk1.5.0_08
 set ANT_HOME=%~d0\apache-ant-1.6.5
-set PATH=%ANT_HOME%\bin;%JAVA_HOME%\bin;%PATH%
+set PATH=%ANT_HOME%\bin;%JAVA_HOME%\bin;%PATH%;%~d0\bin
 set CLASSPATH=
+set CVS_EXT=cvswrap
